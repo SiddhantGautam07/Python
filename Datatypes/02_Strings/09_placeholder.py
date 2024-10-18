51f50567e1988da4f58dc286b2cc8156eada769e
SiddhantGautam07 <gautam620758@gmail.com>
2024-10-18 22:59:28 +0530

    Format Methods

---
+++ Datatypes/02_Strings/09_placeholder.py
@@ -0,0 +1,10 @@
+chai_type = "Masala"
+quantity = 2
+order = "I ordered {} cups of {} chai"
+print(order)
+print()
+
+print(order.format(quantity,chai_type))
+
+
+# format method() :- It is called placeholder , thorugh which we can add another variable.
