4023544af459368bb15629570076807cc76ca8a7
SiddhantGautam07 <gautam620758@gmail.com>
2024-10-20 00:26:16 +0530

    Weather Suggestion

---
+++ 03_Conditionals/05_Solution.py
@@ -0,0 +1,9 @@
+weather = "sunny"
+
+if weather == "sunny":
+    activity = "Go for a Walk"
+elif weather == "Rainy":
+    activity = "Read a book"
+elif weather == "Sonwy":
+    activity = "Build a snowman"
+print(activity)
