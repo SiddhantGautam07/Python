f959d4634aa05e9935e74fb4eb9656da7b8f9ae5
SiddhantGautam07 <gautam620758@gmail.com>
2024-10-19 20:32:16 +0530

    Pop Method

---
+++ Datatypes/04_Dictionary/05_popMethod.py
@@ -0,0 +1,12 @@
+chai_type = {"Masala": "Spicy", "Green": "Mild", "Ginger": "Zesty"}
+print(chai_type)
+print()
+
+chai_type.pop("Ginger")
+print(chai_type)
+print()
+
+del chai_type ["Green"]
+print(chai_type)
+print()
+
