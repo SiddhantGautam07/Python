6bf4548fecdbb4207969227367df5dd298bda3b1
SiddhantGautam07 <gautam620758@gmail.com>
2024-10-19 17:54:09 +0530

    Slicing Method Output

---
+++ Datatypes/03_List/02_SlicingMethod.py
@@ -0,0 +1,16 @@
+name = ["Siddhant", "Gautam", "Gulshan", "Rishav", "Uday", "Raj"]
+print(name)
+print()
+
+print(name[1:3])
+print()
+
+print(name[:3])
+print()
+
+print(name[-3])
+print()
+
+print(name[2:])
+print()
+
