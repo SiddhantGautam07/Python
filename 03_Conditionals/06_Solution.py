4151595b7d20638aea434e1eb51399565593cb19
SiddhantGautam07 <gautam620758@gmail.com>
2024-10-20 00:26:56 +0530

    transportation System Problem

---
+++ 03_Conditionals/06_Solution.py
@@ -0,0 +1,9 @@
+distance = 15
+
+if distance <=3:
+    transportation = "Walk" 
+elif distance <= 15:
+    transportation = "Bike"
+elif distance >15:
+    transportation = "Car"
+print(transportation)
