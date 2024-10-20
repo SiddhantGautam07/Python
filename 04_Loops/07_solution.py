7f91dfdae5081bc229f371e802d9ae1431dcdcc3
SiddhantGautam07 <gautam620758@gmail.com>
2024-10-20 21:11:10 +0530

    Validate Input

---
+++ 04_Loops/07_solution.py
@@ -0,0 +1,7 @@
+while True:
+    number = int(input("Enter the value b/t 1-10 : "))
+    if number >= 1 and number <=10:
+        print("Thanks")
+        break
+    else:
+        print("Invalid Number! Please try again")
