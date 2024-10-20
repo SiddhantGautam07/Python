f6772d8666e719b7cf678b7d00a6a1b566648bbc
SiddhantGautam07 <gautam620758@gmail.com>
2024-10-20 12:43:57 +0530

    Add Method

---
+++ 02_Datatypes/03_List/09_AddMethod.py
@@ -0,0 +1,14 @@
+name = ["Siddhant","Gautam","Rajnish","Uday"]
+print(name)
+print()
+
+name.insert(1,"Pushpank")
+print(name)
+print()
+
+name.insert(2,"Raj")
+print(name)
+print()
+
+# Insert Method() :- It is used to insert the value inside list and It needs two parameters one index poistion and other value that you wants to add.
+
