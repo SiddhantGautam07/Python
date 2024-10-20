32deac087df1767a9bdbbe3015b8d7fc32c1178c
SiddhantGautam07 <gautam620758@gmail.com>
2024-10-20 12:36:55 +0530

    List In String

---
+++ 02_Datatypes/02_Strings/10_ListInString.py
@@ -0,0 +1,13 @@
+chai_variety = ["Lemon", "Masala", "Ginger"]
+print(chai_variety)
+print()
+
+print("".join(chai_variety))
+print()
+
+print(" ".join(chai_variety))
+print()
+
+print(", ".join(chai_variety))
+
+# .Join Method :- It is used to join any list at a points.
