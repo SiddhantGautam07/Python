9e79ae0ddcda18f9994d8fda21eb52d1f33a37ab
SiddhantGautam07 <gautam620758@gmail.com>
2024-10-20 12:44:18 +0530

    Copy Method

---
+++ 02_Datatypes/03_List/10_copyMethod.py
@@ -0,0 +1,26 @@
+charecters = ["Ironman", "Thor", "BlackPanther", "Dr.Stragne", "SpiderMan"]
+print(charecters)
+print()
+
+actors_copy = charecters.copy()
+print(charecters)
+print()
+print(actors_copy)
+
+actors_copy.append("Hawkay")
+print(actors_copy)
+print()
+print(charecters)
+
+superhero = charecters
+print(superhero)
+print()
+
+superhero.insert(1,"Superman",)
+print(superhero)
+print()
+print(charecters)
+
+# copy method() :- This is used to copy any other list to new one
+
+
