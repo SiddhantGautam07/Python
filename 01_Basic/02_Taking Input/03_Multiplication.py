502bb7907a3b3c5af3cf32cdc9ffe8f06ec64db6
SiddhantGautam07 <gautam620758@gmail.com>
2024-10-24 23:58:46 +0530

    Multiplication of two Numbers

---
+++ 01_Basic/02_Taking Input/03_Multiplication.py
@@ -0,0 +1,6 @@
+first = int(input("Enter the first number : "))
+second = int(input("Enter the second number : "))
+
+multiplication = first * second
+
+print("Multiplication of Two Number =",multiplication)
