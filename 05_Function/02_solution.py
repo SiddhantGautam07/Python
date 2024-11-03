1f85f3997020991a4f01ca7a3a779b7532f18115
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-03 18:40:54 +0530

    Function with multiple Parameters

---
+++ 05_Function/02_solution.py
@@ -0,0 +1,8 @@
+First_number = int(input("Enter First Number : "))
+Second_number = int(input("Enter Second Number :"))
+
+def add(First_number,Second_number):
+    return First_number + Second_number
+
+sum = add(First_number,Second_number)
+print(sum)
