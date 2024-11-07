41b28d7c448be608fe086126f51260e5ab274c97
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-07 21:09:16 +0530

    Swapping two numbers using three variables

---
+++ 01_Basic/02_Taking Input/05_SwappingNumbers.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+temp = None
+
+temp = a 
+a = b
+b = temp
+
+print("Value of a =",a)
+
+print("Value of b =",b)
+print("vlaue of temp =",temp)
