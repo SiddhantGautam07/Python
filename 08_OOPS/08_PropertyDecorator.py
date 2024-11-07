c42cbb2473f94dd8c2e192535f20d04a69d4ac70
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-07 12:17:56 +0530

    Property Decorators

---
+++ 08_OOPS/08_PropertyDecorator.py
@@ -0,0 +1,30 @@
+class Car:
+
+    total_car = 0
+
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+        Car.total_car += 1
+    
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+
+    @staticmethod
+    def general_description():
+        return "Car is a means of Transportation"
+
+class ElctricCar (Car):
+    def __init__(self,brand,model,battery_size):
+        super().__init__(brand,model)
+        self.battery_size = battery_size
+
+tesla = ElctricCar("Tesla", "Model S", "100Kwh")
+# print(tesla.brand)
+
+safari = Car("Tata","Safari")
+print(safari.general_description())
+print(Car.general_description())
+
+# new_car = Car("Tata","Nexon")
+
