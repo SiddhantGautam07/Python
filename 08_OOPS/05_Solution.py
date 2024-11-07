3a80bbd62bad9cde172dd3ca876f5a96c0b5aaff
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-07 12:16:48 +0530

    Polymorphism

---
+++ 08_OOPS/05_Solution.py
@@ -0,0 +1,25 @@
+class Car:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+    
+    def full_name (self):
+        return f"{self.brand}{self.model}"
+    
+    def fuel_type(self):
+        return "Petrol or Diesel"
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+    def fuel_type(self):
+        return "Electric Charge"
+
+
+tesla_car = ElectricCar("Tesla", "Model S", "80KWH")
+print(tesla_car.fuel_type())
+
+Safari = Car("Tata", "Safari")
+print(Safari.fuel_type())
