eac3c2efcb666aa95615e0588b3cbfecee04ee02
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-08 15:42:25 +0530

    Count Methods

---
+++ 02_Datatypes/02_Sequences/01_Strings/08_countMethod.py
@@ -0,0 +1,5 @@
+name = "Siddhant gautam siddhant gautam gulshan Siddhant Gautam Raj Gautam Raj Gautam Siddhant Gautam"
+print(name.count("Gautam"))
+
+# Count Method ():- It is used to count the repeated words in strings
+
