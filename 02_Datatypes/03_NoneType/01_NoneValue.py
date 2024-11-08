7085cbee20a597e7860c50539fd47bb7943135db
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-08 21:26:45 +0530

    None Data type

---
+++ 02_Datatypes/03_NoneType/01_NoneValue.py
@@ -0,0 +1,12 @@
+# swapp two numbers
+
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+temp = None
+
+temp = a 
+a = b
+b = temp
+
+print("Value of a After Swapping :",a)
+print("Value of b After Swapping :",b)
