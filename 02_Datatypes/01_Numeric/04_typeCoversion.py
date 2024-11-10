6b154c749c2ee5f25fb97356abeacbb3d86b4b9e
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-10 13:37:43 +0530

    Type Coversion in Python

---
+++ 02_Datatypes/01_Numeric/04_typeCoversion.py
@@ -0,0 +1,22 @@
+#  Integer conversion
+
+x = 10.58
+int(x)
+print(x)
+print(int(x))
+print()
+
+# Float conversion
+
+y = 65
+float(y)
+print(y)
+print(float(y))
+print()
+
+# Complex conversion
+
+a = 10
+b = 5
+print(a,b)
+print(complex(a,b))
