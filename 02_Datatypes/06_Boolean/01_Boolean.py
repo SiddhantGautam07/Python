7ef04a3f3386f06f80c99b7c20efcb63d7ae1f52
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-10 17:06:54 +0530

    Boolean Data Type

---
+++ 02_Datatypes/06_Boolean/01_Boolean.py
@@ -0,0 +1,25 @@
+a = 10 
+b = 20
+if a < b:
+    print("Hello Sir")
+
+print()
+
+a = 10 > 5
+print(a)
+print(type(a))
+print()
+
+a = 5 > 10
+print(a)
+print(type(a))
+print()
+
+b = True + True
+print(b)
+
+c = True - False
+print(c)
+
+d = False - True
+print(d)
