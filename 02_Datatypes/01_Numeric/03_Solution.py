f4964b7eca440fa6a0baf3f06104d79ec1d223ff
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-10 14:06:09 +0530

    Coversion of Deciaml number into Octal, Binary, Hexadecimal Number

---
+++ 02_Datatypes/01_Numeric/03_Solution.py
@@ -0,0 +1,12 @@
+a = 525
+b = bin(a)
+print("Binary number of 525 =",b)
+print()
+
+c = hex(a)
+print("Hexadecimal Number of 525 =",c)
+print()
+
+d = oct(a)
+print("Octal Number of 525 =",d)
+print()
