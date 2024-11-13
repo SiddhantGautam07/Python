5e147e54fcb4ebae132dfd07dd88e8b1c614bf06
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-13 22:19:56 +0530

    Coversion of list into set

---
+++ 02_Datatypes/05_Sets/02_Converting _into_set.py
@@ -0,0 +1,16 @@
+#  We can also use the set() function to create a set as:
+ch = "Hello"
+se = set(ch)
+print(ch)
+print(se)
+print()
+
+
+# we can covert a list into a set using the set() funciton as:
+
+lst = [1,2,3,4,5]
+s = set(lst)
+print(lst)
+print(s)
+# print(s[0])
+
