ee2af40caf63557a23c68860a0aee31e8478dea6
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-22 23:50:39 +0530

    Bitwise Membership Operator

---
+++ 01_Basic/03_Operators/07_Membership_Operator.py
@@ -0,0 +1,11 @@
+names = ["Rani", "Yamini", "Sushmita", "Veena"]
+
+for name in names:
+    print(name)
+print()
+
+postal = {'Delhi': 110001, 'Chennai': 600001, 'kolkata': 700001, 'Banglore': 560001}
+
+for city in postal:
+    print(city, postal[city])
+    
