7b9b6877b9636c38b214b743ef8b6959a239ca30
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-22 14:35:35 +0530

    Relational Operator

---
+++ 01_Basic/03_Operators/04_Relational_Operator.py
@@ -0,0 +1,21 @@
+a = 10
+b = 20
+
+print(a<b)
+print(a>b)
+print(a <= b)
+print(a >= b)
+print(a != b)
+print(a == b)
+print()
+
+if a < b :
+    print("Yes")
+else:
+    print("No")
+
+# Chained Relational Operator
+
+c= 20 > a < 15
+print(c)
+
