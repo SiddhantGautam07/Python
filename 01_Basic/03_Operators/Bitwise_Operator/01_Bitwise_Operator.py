f8a4316ba01166f298caa6d9bafd094e4ec45095
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-22 23:14:25 +0530

    Bitwise Operator Initailize

---
+++ 01_Basic/03_Operators/Bitwise_Operator/01_Bitwise_Operator.py
@@ -0,0 +1,20 @@
+a = int(input("Enter any decimal number : "))
+
+b = bin(a)
+print(b)
+
+b = hex(a)
+print(b)
+
+b = oct(a)
+print(b)
+print()
+
+# Converting Binary to Decimal
+
+# c = bin(input("Enter any binary number : "))
+ 
+# d = hex(c)
+# print(d)
+# print(type(c))
+
