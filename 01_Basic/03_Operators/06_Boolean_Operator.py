3ee7faa4e1431e13e02697de7a54fbf9c72738b2
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-22 14:36:30 +0530

    Bitwise Operator

---
+++ 01_Basic/03_Operators/06_Boolean_Operator.py
@@ -0,0 +1,11 @@
+a = True
+b = False
+
+print("a and a =",a and a)
+print("a and b =",a and b)
+print("b and b =",b and b)
+print("a or a =",a or a)
+print("a or b =",a or b)
+print("b or b =",b or b)
+print(not a)
+print(not b)
