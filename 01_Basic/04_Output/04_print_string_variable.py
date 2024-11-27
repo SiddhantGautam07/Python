cb32e4b12c4e96827075dd8c7abdec21fff9b7d4
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-27 16:27:51 +0530

    Printing Strings Variable

---
+++ 01_Basic/04_Output/04_print_string_variable.py
@@ -0,0 +1,15 @@
+a = 2
+print(a,"is Even Number")
+
+print("You Typed",a,"as Input")
+
+# Use of end
+
+print("Hello",end="")
+print("Dear",end="")
+print("How are U?",end="")
+print()
+
+print("Hello",end="\t")
+print("Dear",end="\t")
+print("How are U?", end="\t")
