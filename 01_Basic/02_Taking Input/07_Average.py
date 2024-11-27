dedb5d527ba72117fb3035e6b31ada138380c24d
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-27 22:38:33 +0530

    Average

---
+++ 01_Basic/02_Taking Input/07_Average.py
@@ -0,0 +1,9 @@
+a = int(input("enter the first number : "))
+b = int(input("enter the second number : "))
+c = int(input("enter the third number : "))
+
+print("The average of {} and {} and {} is {}".format(a,b,c, (a + b + c)/3))
+print()
+print("The Average of ",a,"and",b,"and",c,"is",(a + b + c)/3)
+print()
+print("The Average of {0} and {1} and {2} is {3}".format(a,b,c,(a+b+c)/3))
