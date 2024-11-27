491092dcbc4f4d9d0654d858b7a780258afffded
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-27 16:28:10 +0530

    Formatted String

---
+++ 01_Basic/04_Output/05_Formatted_String.py
@@ -0,0 +1,15 @@
+x = 10
+print("value = %d" % x)
+print("value = %i" % x)
+print("Value = %f" % x)
+print("Value = %s" % x)
+print()
+
+y = 15
+
+print("x =%d y =%i"% (x,y))
+
+name = "Linda"
+print("Hai %s"% name)
+print("Hai [%20s]"% name)
+print("Hai (%-20s)"% name)
