8f6a7a4df5879ce1de996cf6919a0e0c2789044f
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-28 16:30:54 +0530

    Numbers between 1 to 10

---
+++ 03_Conditionals/16_Number_between1and10.py
@@ -0,0 +1,6 @@
+num = int(input("Enter any number : "))
+
+if num >= 1 and num <= 10:
+    print("You Typed",num, "Which is between 1 and 10.")
+else:
+    print("You typed", num, "Which is below 1 or above 10.")
