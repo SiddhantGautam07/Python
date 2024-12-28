cc70fd3b665577ca3204361178fb6190249aacf6
SiddhantGautam07 <gautam620758@gmail.com>
2024-12-28 21:54:08 +0530

    Importing the numpy library

---
+++ 10_Array/Numpy/02_import_numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([10,20,30,40,50])
+print(a)
+b = np.array([10.8,20,30,40,50])
+print(b)
+
+str = np.array(['Delhi','Hydrabad','Punjab','Pune','Chennai','Mumbai'])
+print(str)
