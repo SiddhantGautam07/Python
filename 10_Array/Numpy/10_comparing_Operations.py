2de7859cd9febcf800cb74ee078c59b874dc1b12
SiddhantGautam07 <gautam620758@gmail.com>
2024-12-28 21:58:27 +0530

    Comparision Operation on Array

---
+++ 10_Array/Numpy/10_comparing_Operations.py
@@ -0,0 +1,11 @@
+from numpy import *
+
+x = array([10,20,40,30])
+y = array([20,10,40,50])
+
+z = x == y
+print("Result of x == y =",z)
+z = x > y
+print("Result of x > y =",z)
+z = x < y
+print("Result of x < y =",z)
