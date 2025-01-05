b70f19e2a481e69877b6194ab912197291b1ba16
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-05 18:25:16 +0530

    Indexing in 3 Dimensional array

---
+++ 10_Array/Numpy/27_Indexing_in_3D_array.py
@@ -0,0 +1,16 @@
+from numpy import *
+
+a = [[[1,2,3],
+      [4,5,6]],
+      
+      [[7,8,9],
+       [10,11,12]]]
+print(a)
+print()
+
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        for k in range(len(a[i][j])):
+            print(a[i][j][k],end=" \t")
+        print()
+    # print()
