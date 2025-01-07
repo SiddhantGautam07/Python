02e93c065bc0db05ca5af3b75163fb3ef7c74762
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-07 18:41:59 +0530

    Slicing in 5D Array

---
+++ 10_Array/Numpy/29_Slicing_in_5D_array.py
@@ -0,0 +1,17 @@
+from numpy import *
+
+a = reshape(arange(11,36,1),(5,5))
+print(a)
+print()
+
+print(a[0:2,0:3])
+print()
+print(a[2:4,3:])
+print()
+print(a[0:2,2:])
+print()
+print(a[2:,3:])
+print()
+print()
+print()
+print()
