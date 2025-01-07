fe10aa3da2a68babbab52e772c007ad134d17d80
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-07 18:50:20 +0530

    Same rows and columns matrix calculation

---
+++ 10_Array/Numpy/38_Same_Matrix_Calculation.py
@@ -0,0 +1,25 @@
+from numpy import *
+
+a = matrix("1,2,3; 4,5,6")
+b = matrix("2,2,2; 1,-1,2")
+
+print(a)
+print()
+
+print(b)
+print()
+
+c = a + b
+print(c)
+print()
+
+d = a - b
+print(d)
+print()
+
+e = a / b
+print(e)
+print()
+
+# f = a * b
+# print(f)
