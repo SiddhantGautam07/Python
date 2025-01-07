3e992ba71ad0f612fd41cff9db12f14d17f15ad6
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-07 18:43:57 +0530

    Finding Sum and Average Value

---
+++ 10_Array/Numpy/33_Sum_and_Average.py
@@ -0,0 +1,14 @@
+from numpy import *
+
+a = matrix("1,2,3; 4,5,6; 7,8,9")
+print(a)
+print()
+
+# Sum of elements
+sum = a.sum()
+print("sum of Elements =",sum)
+print()
+
+# Average of Elements
+avg = a.mean()
+print("Average of Elements =",avg)
