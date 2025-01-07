3ca195124dcc763be5d9a23a87dac86adc926def
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-07 18:54:35 +0530

    Random Function

---
+++ 10_Array/Numpy/41_Random_funciton.py
@@ -0,0 +1,17 @@
+from numpy import *
+
+a = random.rand(10)
+print(a)
+print()
+
+b = random.rand(2,3)
+print(b)
+print()
+
+c = random. rand(3,4)
+print(c)
+print()
+
+d = random.rand(3,2)
+print(d)
+print()
