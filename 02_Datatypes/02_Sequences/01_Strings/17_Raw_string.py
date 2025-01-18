89bfda9f6c45daf13421f08e7a890eb2fb2e9de6
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-18 19:36:56 +0530

    Raw String

---
+++ 02_Datatypes/02_Sequences/01_Strings/17_Raw_string.py
@@ -0,0 +1,13 @@
+a = r"Welcome to \nCore Python\t Learning"
+print(a)
+
+name = u"\u0915\u094b\u0930 \u092a\u0948\u0925\u0964\u0928"
+print(name)
+
+n = len(a)
+print(n)
+
+b = len(name)
+print(b)
+
+
