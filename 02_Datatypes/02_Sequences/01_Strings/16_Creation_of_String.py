9bb29483935ee067d79c74381092119a8cc30d3f
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-18 19:36:31 +0530

    Creation of String

---
+++ 02_Datatypes/02_Sequences/01_Strings/16_Creation_of_String.py
@@ -0,0 +1,14 @@
+str = "Welcom to Python"
+print(str)
+
+a = "Welcome to 'Python Programming' Learning"
+print(a)
+
+b = "welcome to \tPython Programming\n Learning"
+print(b)
+
+c = "Welcome to\a Python Programming\v Learning"
+print(c)
+
+d = "Welcome to \r Python Programming \r Learning"
+print(d)
