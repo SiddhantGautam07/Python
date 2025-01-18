d3acc6cdb904406247fb12cc9e48686d40e57085
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-18 19:35:57 +0530

    Strip Methods in python

--- 02_Datatypes/02_Sequences/01_Strings/04_StripMethods.py
+++ 02_Datatypes/02_Sequences/01_Strings/04_StripMethods.py
@@ -1,7 +1,10 @@
 name = "           Siddhant Gautam        "
 print(name)
 print()
-
+print(name.lstrip())
+print()
+print(name.rstrip())
+print()
 print(name.strip())
 
 # Strip() is used to remove the blank space 
