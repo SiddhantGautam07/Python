a05d3c4be178777f5e033b90c75ba539264b6385
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-22 20:19:08 +0530

    Checking Membership

---
+++ 02_Datatypes/02_Sequences/01_Strings/19_checking_Membership.py
@@ -0,0 +1,8 @@
+str = input("Enter main String :")
+sub = input("Enter sub String : ")
+
+if sub in str:
+    print(sub+" is Found in main String")
+else:
+    print(sub+" is not found in the main string")
+
