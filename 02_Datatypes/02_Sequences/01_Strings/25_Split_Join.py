ea9e7831fa87ca7c04b160720288229dc6701f88
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-24 23:21:03 +0530

    Split and join in String

---
+++ 02_Datatypes/02_Sequences/01_Strings/25_Split_Join.py
@@ -0,0 +1,15 @@
+str = input("Enter the any String : ")
+
+# Spliting The Strings
+lst = str.split(' ')
+
+for i in lst:
+    print(i)
+
+# Join The String
+
+new = input("Enter the new String : ")
+sep = ":"
+str1 = sep.join(new)
+print(str1)
+
