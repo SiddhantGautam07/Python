21faaf0f135195b7f0fedd1a58d9d7694c6fb5fd
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-24 23:21:54 +0530

    Startswith and EndsWith in String

---
+++ 02_Datatypes/02_Sequences/01_Strings/27_Startwith_endwith.py
@@ -0,0 +1,13 @@
+# Startswith() methods
+
+str = input("Enter any String : ")
+start = input("Please check the first input : ")
+new = str.startswith(start)
+print(new)
+print()
+
+# Endswith() methods
+end = input("Check last value : ")
+new1 = str.endswith(end)
+print(new1)
+
