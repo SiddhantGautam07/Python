c40acba2797498898373305e94c00b1b8de0b427
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-24 23:21:24 +0530

    change Case in String

---
+++ 02_Datatypes/02_Sequences/01_Strings/26.Change_Case_in_String.py
@@ -0,0 +1,21 @@
+str = input("Enter any Strings : ")
+
+# for Upper Case in Python
+upp = str.upper()
+print(upp)
+print()
+
+# for Lower Case in Python
+lower = str. lower()
+print(lower)
+print()
+
+# for Swap Case in Python
+swap = str.swapcase()
+print(swap)
+print()
+
+# for title Case in Python
+title = str.title()
+print(title)
+
