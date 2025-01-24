6cc6112b805e5ad3066742611663d9f41fe03c15
SiddhantGautam07 <gautam620758@gmail.com>
2025-01-24 23:22:10 +0530

    Testing the String

---
+++ 02_Datatypes/02_Sequences/01_Strings/28_testing_string.py
@@ -0,0 +1,35 @@
+row = 'isDelhi999'
+
+# isalnum() method
+isalnum = row.isalnum()
+print(isalnum)
+print()
+
+# isalpha() method
+isalpha = row.isalpha()
+print(isalpha)
+print()
+
+# isdigit() methdo
+isdigit = row.isdigit()
+print(isdigit)
+print()
+
+# islower() method 
+islower = row.islower()
+print(islower)
+print()
+
+# isupper() method
+isupper = row.isupper()
+print(isupper)
+print()
+
+# istitle() method
+istitle = row.istitle()
+print(istitle)
+print()
+
+# isspace() method
+isspace = row.isspace()
+print(isspace)
