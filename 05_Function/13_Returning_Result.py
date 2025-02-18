6400b4610e87228e65a784c5435cc733536c1a35
Siddhantgautam07 <gautam620758@gmail.com>
2025-02-18 21:03:47 +0530

    Returing Results

---
+++ 05_Function/13_Returning_Result.py
@@ -0,0 +1,13 @@
+def sum(a,b):
+    """This is Sum of two numbers"""
+    c = a + b
+    return c
+
+d = sum(10,20)
+print("The sum of d =",d)
+e = sum(30.23,20.34)
+print("The sum of e =",d)
+
+
+
+
